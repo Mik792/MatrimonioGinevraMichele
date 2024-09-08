c22a64652522bee2b621ca862b64649dc33c12f7
Mik792 <115691194+Mik792@users.noreply.github.com>
2024-09-08 11:35:21 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Conto alla rovescia per il giorno del matrimonio
+const countDownDate = new Date("Sep 15, 2024 16:00:00").getTime();
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+    
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "L'evento è iniziato!";
+    }
+}, 1000);
